8e9ecfc97f0fe1aef22bea4bb02778af67e6a91d
Androidmatis <androidmatis@gmail.com>
2025-01-16 08:11:26 +0600

    JDBC (Стейтменты)

--- src/main/java/io/hexlet/Application.java
+++ src/main/java/io/hexlet/Application.java
@@ -10,13 +10,23 @@ public class Application {
             try (var statement = conn.createStatement()) {
                 statement.execute(sql);
             }
-            var sql2 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789')";
-            try (var statement2 = conn.createStatement()) {
-                statement2.executeUpdate(sql2);
+            var sql2 = "INSERT INTO users (username, phone) VALUES (?, ?)";
+            try (var preparedStatement = conn.prepareStatement(sql2)) {
+                preparedStatement.setString(1, "Tommy");
+                preparedStatement.setString(2, "841343");
+                preparedStatement.executeUpdate();
+                preparedStatement.setString(1, "Marry");
+                preparedStatement.setString(2, "835124");
+                preparedStatement.executeUpdate();
             }
-            var sql3 = "SELECT * FROM users";
-            try (var statement3 = conn.createStatement()) {
-                var resultSet = statement3.executeQuery(sql3);
+            var sql3 = "DELETE FROM users WHERE username = ?";
+            try (var preparedStatement = conn.prepareStatement(sql3)) {
+                preparedStatement.setString(1, "Tommy");
+                preparedStatement.execute();
+            }
+            var sql4 = "SELECT * FROM users";
+            try (var statement4 = conn.createStatement()) {
+                var resultSet = statement4.executeQuery(sql4);
                 while (resultSet.next()) {
                     System.out.println(resultSet.getString("username"));
                     System.out.println(resultSet.getString("phone"));
